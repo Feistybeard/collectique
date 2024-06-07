62af910ebc1fc65ccac6f7b219bc5d6cb55b74c9
Marvan Al Kufai <alkufai@gmail.com>
2024-06-07 15:52:03 +0200

    feat: login page (ui)

--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -1,26 +1,48 @@
+import { Link } from 'react-router-dom';
+
 import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
-function Login() {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate('/start');
-    console.log('Loging in...');
-  };
-
+export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <Button
-        onClick={() => {
-          handleLogin();
-        }}
-      >
-        Log In
-      </Button>
-    </div>
+    <>
+      <h1>Collectique Login</h1>
+      <Card className='mx-auto max-w-sm'>
+        <CardHeader>
+          <CardTitle className='text-2xl'>Login</CardTitle>
+          <CardDescription>Enter your email below to login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='grid gap-4'>
+            <div className='grid gap-2'>
+              <div className='flex items-center'>
+                <Label htmlFor='email'>Email</Label>
+              </div>
+              <Input id='email' type='email' required />
+            </div>
+            <div className='grid gap-2'>
+              <div className='flex items-center'>
+                <Label htmlFor='password'>Password</Label>
+              </div>
+              <Input id='password' type='password' required />
+            </div>
+            <Button type='submit' className='w-full'>
+              Login
+            </Button>
+          </div>
+          <Link to='#' className='ml-auto inline-block text-sm underline'>
+            Forgot your password?
+          </Link>
+          <div className='mt-4 text-center text-sm'>
+            Don&apos;t have an account?{' '}
+            <Link to='/register' className='underline'>
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
-
-export default Login;
