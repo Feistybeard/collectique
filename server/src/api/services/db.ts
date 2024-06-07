e053759565dc9695461c0bb235413af870262cf1
Marvan Al Kufai <alkufai@gmail.com>
2024-06-07 15:46:39 +0200

    fix: update db connection config

--- server/src/api/services/db.ts
+++ server/src/api/services/db.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import { DB_CONFIG } from "../../config";
+import { log } from "console";
 
 const db = async () => {
   await mongoose
     .set("strictQuery", false)
-    .connect(`mongodb://${DB_CONFIG.host}:${DB_CONFIG.port}/${DB_CONFIG.name}`)
+    .connect(
+      `mongodb://${DB_CONFIG.username}:${DB_CONFIG.password}@${DB_CONFIG.host}:${DB_CONFIG.port}/${DB_CONFIG.name}?authSource=${DB_CONFIG.authsource}`
+    )
     .then(() => console.log("Successfully connected to MongoDB"))
     .catch((err) => console.error(err));
 };
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -6,6 +6,9 @@ export const DB_CONFIG = {
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 27017,
   name: process.env.DB_NAME || "collectique",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  authsource: process.env.DB_AUTH_SOURCE,
 };
 
 export const JWT_SECRET = process.env.JWT_SECRET;
